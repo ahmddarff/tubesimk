9ecea7f2e2b8a62a2b9e6a0f5c4bdfee491f6fef
RahmaSarita <raanasution7@gmail.com>
2026-04-23 09:46:11 +0700

    Dashboard kasir

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return "Hello Terralog!"
 
+@app.route('/kasir')
+def kasir_dashboard():
+    katalog_menu = [
+        {"nama": "Terralog Kopi", "harga": 18000, "img": "kopi.png", "rating": 4.7, "terjual": 11, "status": "tersedia"},
+        {"nama": "Espresso", "harga": 10000, "img": "kopi.png", "rating": 4.8, "terjual": 5, "status": "tersedia"},
+        {"nama": "Sanger", "harga": 18000, "img": "kopi.png", "rating": 4.6, "terjual": 4, "status": "tersedia"},
+        {"nama": "Americano", "harga": 15000, "img": "kopi.png", "rating": 4.3, "terjual": 8, "status": "tersedia"},
+        {"nama": "Cappuccino", "harga": 18000, "img": "kopi.png", "rating": 4.5, "terjual": 5, "status": "tersedia"},
+        {"nama": "Kopi Latte", "harga": 16000, "img": "kopi.png", "rating": 4.3, "terjual": 4, "status": "habis"},
+    ]
+    
+    return render_template('kasir/dashboard.html', menu=katalog_menu, segment='dashboard')
 if __name__ == '__main__':
     app.run(debug=True, port=50001)
