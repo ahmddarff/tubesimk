bca3c842eb75656aabaa9f5c41f35a80010382f6
RahmaSarita <raanasution7@gmail.com>
2026-04-30 11:14:28 +0700

    Tab pesanan_aktif

--- app.py
+++ app.py
@@ -16,7 +16,24 @@ def kasir_dashboard():
         {"nama": "Cappuccino", "harga": 18000, "img": "kopi.png", "rating": 4.5, "terjual": 5, "status": "tersedia"},
         {"nama": "Kopi Latte", "harga": 16000, "img": "kopi.png", "rating": 4.3, "terjual": 4, "status": "habis"},
     ]
-    
     return render_template('kasir/dashboard.html', menu=katalog_menu, segment='dashboard')
+    
+
+@app.route('/pesanan-aktif')
+def pesanan_aktif():
+    # Data contoh untuk pesanan
+    pesanan_list = [
+        {"id": "P052", "nama": "Agnes", "meja": "07", "status": "PENDING", "total": 48000, "waktu": "12.12", "tipe": "DINE IN"},
+        {"id": "P051", "nama": "Joy", "meja": "-", "status": "PENDING", "total": 48000, "waktu": "11.56", "tipe": "TAKE AWAY"},
+        {"id": "P049", "nama": "Rahma", "meja": "06", "status": "READY", "total": 48000, "waktu": "11.27", "tipe": "DINE IN"},
+        {"id": "P045", "nama": "Sonya", "meja": "03", "status": "SERVED", "total": 48000, "waktu": "10.40", "tipe": "DINE IN"},
+        {"id": "P051", "nama": "Joy", "meja": "-", "status": "PENDING", "total": 48000, "waktu": "11.56", "tipe": "TAKE AWAY"},
+        {"id": "P049", "nama": "Rahma", "meja": "06", "status": "READY", "total": 48000, "waktu": "11.27", "tipe": "DINE IN"},
+        {"id": "P045", "nama": "Sonya", "meja": "03", "status": "SERVED", "total": 48000, "waktu": "10.40", "tipe": "DINE IN"},
+        
+    ]
+    return render_template('kasir/pesanan_aktif.html', segment='pesanan_aktif', pesanan=pesanan_list)
+    
+    
 if __name__ == '__main__':
     app.run(debug=True, port=50001)
