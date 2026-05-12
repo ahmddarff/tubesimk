ca4a0b7eb4bcb8a1abd9600ad31ff34b8736f2ab
Richard <richardlcp119@gmail.com>
2026-05-12 12:50:39 +0700

    .

--- models.py
+++ models.py
@@ -29,6 +29,7 @@ class CafeSetting(db.Model):
     __tablename__ = 'cafe_settings'
     id                      = db.Column(db.Integer, primary_key=True)
     cafe_name               = db.Column(db.String(100), default="Terralog Coffee & Eatery")
+    logo                    = db.Column(db.String(255), nullable=True)
     phone                   = db.Column(db.String(20), nullable=True)
     email                   = db.Column(db.String(100), nullable=True)
     address                 = db.Column(db.Text, nullable=True)
@@ -66,6 +67,7 @@ class Menu(db.Model):
     description     = db.Column(db.Text, nullable=True)
     price           = db.Column(db.Integer, nullable=False)
     image_url       = db.Column(db.String(255), nullable=True)
+    stock           = db.Column(db.Integer, nullable=True) # Boleh NULL untuk makanan yang dimasak
     is_available    = db.Column(db.Boolean, default=True)
 
     created_at      = db.Column(db.DateTime, default=datetime.utcnow)
